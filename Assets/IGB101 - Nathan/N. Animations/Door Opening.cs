1b45bd15a4ebb351c50a4e1163f7d558deffc7c6
Derby2004 <ncrib2004@gmail.com>
2025-05-29 23:07:59 +1000

    Changed doors and added fireplace
    Added fireplace
    Added more doors
    Changed door texture and added door frame
    added more rooms and restructured areas
    changed animation for doors

--- Assets/IGB101 - Nathan/N. Animations/Door Opening.cs
+++ Assets/IGB101 - Nathan/N. Animations/Door Opening.cs
@@ -1,18 +1,23 @@
+using NUnit.Framework;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class DoorOpening : MonoBehaviour
 {
     Animation animation;
+
     // Use this for initialization
     void Start()
     {
         animation = GetComponent<Animation>();
     }
+
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown("f"))
             animation.Play();
     }
+
 
 }
